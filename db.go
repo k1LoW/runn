df999b39d5a9b091c92b6554b595c4e1925bd99d
k1LoW <k1lowxb@gmail.com>
2022-03-10 10:20:48 +0900

    dbRunner support multiple queries

--- db.go
+++ db.go
@@ -35,92 +35,137 @@ func newDBRunner(name, dsn string, o *operator) (*dbRunner, error) {
 }
 
 func (rnr *dbRunner) Run(ctx context.Context, q *dbQuery) error {
-	if rnr.operator.debug {
-		_, _ = fmt.Fprintf(os.Stderr, "-----START QUERY-----\n%s-----END QUERY-----\n", q.stmt)
-	}
-	if !strings.HasPrefix(strings.ToUpper(q.stmt), "SELECT") {
-		r, err := rnr.client.ExecContext(ctx, q.stmt)
-		if err != nil {
-			return err
+	stmts := separateStmt(q.stmt)
+	out := map[string]interface{}{}
+	for _, stmt := range stmts {
+		if rnr.operator.debug {
+			_, _ = fmt.Fprintf(os.Stderr, "-----START QUERY-----\n%s-----END QUERY-----\n", stmt)
 		}
-		id, err := r.LastInsertId()
-		if err != nil {
-			return err
-		}
-		a, err := r.RowsAffected()
-		if err != nil {
-			return err
-		}
-		rnr.operator.store.steps = append(rnr.operator.store.steps, map[string]interface{}{
-			"last_insert_id": id,
-			"raws_affected":  a,
-		})
-		return nil
-	}
-	rows := []map[string]interface{}{}
-	r, err := rnr.client.QueryContext(ctx, q.stmt)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-
-	columns, err := r.Columns()
-	if err != nil {
-		return err
-	}
-	for r.Next() {
-		row := map[string]interface{}{}
-		vals := make([]interface{}, len(columns))
-		valsp := make([]interface{}, len(columns))
-		for i := range columns {
-			valsp[i] = &vals[i]
-		}
-		if err := r.Scan(valsp...); err != nil {
-			return err
-		}
-		for i, c := range columns {
-			switch v := vals[i].(type) {
-			case []byte:
-				s := string(v)
-				num, err := strconv.Atoi(s)
-				if err == nil {
-					row[c] = num
-				} else {
-					row[c] = s
+		err := func() error {
+			if !strings.HasPrefix(strings.ToUpper(stmt), "SELECT") {
+				// exec
+				r, err := rnr.client.ExecContext(ctx, stmt)
+				if err != nil {
+					return err
 				}
-			default:
-				row[c] = v
+				id, err := r.LastInsertId()
+				if err != nil {
+					return err
+				}
+				a, err := r.RowsAffected()
+				if err != nil {
+					return err
+				}
+				out = map[string]interface{}{
+					"last_insert_id": id,
+					"raws_affected":  a,
+				}
+				return nil
 			}
-		}
-		rows = append(rows, row)
-	}
-	if err := r.Err(); err != nil {
-		return err
-	}
-	if rnr.operator.debug {
-		_, _ = fmt.Fprintln(os.Stderr, "-----START ROWS-----")
-		table := tablewriter.NewWriter(os.Stderr)
-		table.SetHeader(columns)
-		table.SetAutoFormatHeaders(false)
-		table.SetAutoWrapText(false)
-		for _, r := range rows {
-			row := make([]string, 0, len(columns))
-			for _, c := range columns {
-				row = append(row, fmt.Sprintf("%v", r[c]))
+
+			// query
+			rows := []map[string]interface{}{}
+			r, err := rnr.client.QueryContext(ctx, stmt)
+			if err != nil {
+				return err
 			}
-			table.Append(row)
+			defer r.Close()
+
+			columns, err := r.Columns()
+			if err != nil {
+				return err
+			}
+			for r.Next() {
+				row := map[string]interface{}{}
+				vals := make([]interface{}, len(columns))
+				valsp := make([]interface{}, len(columns))
+				for i := range columns {
+					valsp[i] = &vals[i]
+				}
+				if err := r.Scan(valsp...); err != nil {
+					return err
+				}
+				for i, c := range columns {
+					switch v := vals[i].(type) {
+					case []byte:
+						s := string(v)
+						num, err := strconv.Atoi(s)
+						if err == nil {
+							row[c] = num
+						} else {
+							row[c] = s
+						}
+					default:
+						row[c] = v
+					}
+				}
+				rows = append(rows, row)
+			}
+			if err := r.Err(); err != nil {
+				return err
+			}
+			if rnr.operator.debug {
+				_, _ = fmt.Fprintln(os.Stderr, "-----START ROWS-----")
+				table := tablewriter.NewWriter(os.Stderr)
+				table.SetHeader(columns)
+				table.SetAutoFormatHeaders(false)
+				table.SetAutoWrapText(false)
+				for _, r := range rows {
+					row := make([]string, 0, len(columns))
+					for _, c := range columns {
+						row = append(row, fmt.Sprintf("%v", r[c]))
+					}
+					table.Append(row)
+				}
+				table.Render()
+				c := len(rows)
+				if c == 1 {
+					_, _ = fmt.Fprintf(os.Stderr, "(%d row)\n", len(rows))
+				} else {
+					_, _ = fmt.Fprintf(os.Stderr, "(%d rows)\n", len(rows))
+				}
+				_, _ = fmt.Fprintln(os.Stderr, "-----END ROWS-----")
+			}
+			out = map[string]interface{}{
+				"rows": rows,
+			}
+			return nil
+		}()
+		if err != nil {
+			return err
 		}
-		table.Render()
-		c := len(rows)
-		if c == 1 {
-			_, _ = fmt.Fprintf(os.Stderr, "(%d row)\n", len(rows))
-		} else {
-			_, _ = fmt.Fprintf(os.Stderr, "(%d rows)\n", len(rows))
-		}
-		_, _ = fmt.Fprintln(os.Stderr, "-----END ROWS-----")
 	}
-	rnr.operator.store.steps = append(rnr.operator.store.steps, map[string]interface{}{
-		"rows": rows,
-	})
+	rnr.operator.store.steps = append(rnr.operator.store.steps, out)
 	return nil
+}
+
+func separateStmt(stmt string) []string {
+	if !strings.Contains(stmt, ";") {
+		return []string{stmt}
+	}
+	stmts := []string{}
+	s := []rune{}
+	ins := false
+	ind := false
+	for _, c := range stmt {
+		s = append(s, c)
+		switch c {
+		case '\'':
+			ins = !ins
+		case '"':
+			ind = !ind
+		case ';':
+			if !ins && !ind {
+				stmts = append(stmts, strings.Trim(string(s), " \n"))
+				s = []rune{}
+			}
+		}
+	}
+	if len(s) > 0 {
+		l := strings.Trim(string(s), " \n")
+		if len(l) > 0 {
+			stmts = append(stmts, l)
+		}
+	}
+	return stmts
 }
--- db_test.go
+++ db_test.go
@@ -1,7 +1,164 @@
 package runn
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func TestDBRun(t *testing.T) {
+	tests := []struct {
+		stmt string
+		want map[string]interface{}
+	}{
+		{
+			"SELECT 1",
+			map[string]interface{}{
+				"rows": []map[string]interface{}{
+					{"1": int64(1)},
+				},
+			},
+		},
+		{
+			"SELECT 1;SELECT 2;",
+			map[string]interface{}{
+				"rows": []map[string]interface{}{
+					{"2": int64(2)},
+				},
+			},
+		},
+		{
+			`CREATE TABLE users (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          username TEXT UNIQUE NOT NULL,
+          password TEXT NOT NULL,
+          email TEXT UNIQUE NOT NULL,
+          created NUMERIC NOT NULL,
+          updated NUMERIC
+        );
+INSERT INTO users (username, password, email, created) VALUES ('alice', 'passw0rd', 'alice@example.com', datetime('2017-12-05'));`,
+			map[string]interface{}{
+				"last_insert_id": int64(1),
+				"raws_affected":  int64(1),
+			},
+		},
+		{
+			`CREATE TABLE users (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          username TEXT UNIQUE NOT NULL,
+          password TEXT NOT NULL,
+          email TEXT UNIQUE NOT NULL,
+          created NUMERIC NOT NULL,
+          updated NUMERIC
+        );
+INSERT INTO users (username, password, email, created) VALUES ('alice', 'passw0rd', 'alice@example.com', datetime('2017-12-05'));
+SELECT COUNT(*) AS count FROM users;
+`,
+			map[string]interface{}{
+				"rows": []map[string]interface{}{
+					{"count": int64(1)},
+				},
+			},
+		},
+	}
+	ctx := context.Background()
+	for _, tt := range tests {
+		func() {
+			db, err := os.CreateTemp("", "tmp")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.Remove(db.Name())
+			o, err := New()
+			if err != nil {
+				t.Fatal(err)
+			}
+			dsn := fmt.Sprintf("sqlite://%s", db.Name())
+			r, err := newDBRunner("db", dsn, o)
+			if err != nil {
+				t.Fatal(err)
+			}
+			q := &dbQuery{stmt: tt.stmt}
+			if err := r.Run(ctx, q); err != nil {
+				t.Error(err)
+				return
+			}
+			got := o.store.steps[0]
+			if diff := cmp.Diff(got, tt.want, nil); diff != "" {
+				t.Errorf("%s", diff)
+			}
+		}()
+	}
+}
 
+func TestSeparateStmt(t *testing.T) {
+	tests := []struct {
+		stmt string
+		want []string
+	}{
+		{
+			"SELECT 1",
+			[]string{"SELECT 1"},
+		},
+		{
+			"SELECT 1;SELECT 2;",
+			[]string{"SELECT 1;", "SELECT 2;"},
+		},
+		{
+			`CREATE TABLE users (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          username TEXT UNIQUE NOT NULL,
+          password TEXT NOT NULL,
+          email TEXT UNIQUE NOT NULL,
+          created NUMERIC NOT NULL,
+          updated NUMERIC
+        );
+INSERT INTO users (username, password, email, created) VALUES ('alice', 'passw0rd', 'alice@example.com', datetime('2017-12-05'));`,
+			[]string{
+				`CREATE TABLE users (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          username TEXT UNIQUE NOT NULL,
+          password TEXT NOT NULL,
+          email TEXT UNIQUE NOT NULL,
+          created NUMERIC NOT NULL,
+          updated NUMERIC
+        );`,
+				"INSERT INTO users (username, password, email, created) VALUES ('alice', 'passw0rd', 'alice@example.com', datetime('2017-12-05'));",
+			},
+		},
+		{
+			`CREATE TABLE users (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          username TEXT UNIQUE NOT NULL,
+          password TEXT NOT NULL,
+          email TEXT UNIQUE NOT NULL,
+          created NUMERIC NOT NULL,
+          updated NUMERIC
+        );
+INSERT INTO users (username, password, email, created) VALUES ('alice', 'passw0rd', 'alice@example.com', datetime('2017-12-05'));
+SELECT COUNT(*) AS count FROM users;
+`,
+			[]string{
+				`CREATE TABLE users (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          username TEXT UNIQUE NOT NULL,
+          password TEXT NOT NULL,
+          email TEXT UNIQUE NOT NULL,
+          created NUMERIC NOT NULL,
+          updated NUMERIC
+        );`,
+				"INSERT INTO users (username, password, email, created) VALUES ('alice', 'passw0rd', 'alice@example.com', datetime('2017-12-05'));",
+				"SELECT COUNT(*) AS count FROM users;",
+			},
+		},
+	}
+	for _, tt := range tests {
+		got := separateStmt(tt.stmt)
+		if diff := cmp.Diff(got, tt.want, nil); diff != "" {
+			t.Errorf("%s", diff)
+		}
+	}
 }

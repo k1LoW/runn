ad7d143c5606725055892184976cabc1f5734bd4
k1LoW <k1lowxb@gmail.com>
2022-07-06 06:52:23 +0900

    Parallelizing gRPC tests

--- grpc_test.go
+++ grpc_test.go
@@ -225,6 +225,7 @@ func TestGrpcRunner(t *testing.T) {
 	ctx := context.Background()
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			ts := grpcstub.NewServer(t, []string{}, "testdata/grpctest.proto")
 			t.Cleanup(func() {
 				ts.Close()

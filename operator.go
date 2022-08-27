ea39bbfd5263ac8b5494eb5a0c30dd701aa7d75f
k1LoW <k1lowxb@gmail.com>
2022-08-27 10:33:19 +0900

    bonsai

--- operator.go
+++ operator.go
@@ -73,67 +73,6 @@ func (s *step) ids() []string {
 	return ids
 }
 
-const (
-	storeVarsKey   = "vars"
-	storeStepsKey  = "steps"
-	storeParentKey = "parent"
-	storeFuncValue = "[func]"
-)
-
-type store struct {
-	steps      []map[string]interface{}
-	stepMaps   map[string]interface{}
-	vars       map[string]interface{}
-	funcs      map[string]interface{}
-	bindVars   map[string]interface{}
-	parentVars map[string]interface{}
-	useMap     bool // Use map syntax in `steps:`.
-	loopIndex  *int
-}
-
-func (s *store) toNormalizedMap() map[string]interface{} {
-	store := map[string]interface{}{}
-	for k := range s.funcs {
-		store[k] = storeFuncValue
-	}
-	store[storeVarsKey] = s.vars
-	if s.useMap {
-		store[storeStepsKey] = s.stepMaps
-	} else {
-		store[storeStepsKey] = s.steps
-	}
-	for k, v := range s.bindVars {
-		store[k] = v
-	}
-	if s.loopIndex != nil {
-		store[loopCountVarKey] = *s.loopIndex
-	}
-	return store
-}
-
-func (s *store) toMap() map[string]interface{} {
-	store := map[string]interface{}{}
-	for k, v := range s.funcs {
-		store[k] = v
-	}
-	store[storeVarsKey] = s.vars
-	if s.useMap {
-		store[storeStepsKey] = s.stepMaps
-	} else {
-		store[storeStepsKey] = s.steps
-	}
-	if s.parentVars != nil {
-		store[storeParentKey] = s.parentVars
-	}
-	for k, v := range s.bindVars {
-		store[k] = v
-	}
-	if s.loopIndex != nil {
-		store[loopCountVarKey] = *s.loopIndex
-	}
-	return store
-}
-
 type operator struct {
 	id          string
 	httpRunners map[string]*httpRunner
@@ -687,7 +626,7 @@ func (o *operator) runInternal(ctx context.Context) error {
 	// if
 	if o.cond != "" {
 		store := o.store.toMap()
-		store["included"] = o.included
+		store[storeIncludedKey] = o.included
 		tf, err := evalCond(o.cond, store)
 		if err != nil {
 			return err
@@ -720,7 +659,7 @@ func (o *operator) runInternal(ctx context.Context) error {
 			}
 			if s.cond != "" {
 				store := o.store.toMap()
-				store["included"] = o.included
+				store[storeIncludedKey] = o.included
 				tf, err := evalCond(s.cond, store)
 				if err != nil {
 					return err
---
+++ store.go
@@ -0,0 +1,63 @@
+package runn
+
+const (
+	storeVarsKey     = "vars"
+	storeStepsKey    = "steps"
+	storeParentKey   = "parent"
+	storeIncludedKey = "included"
+	storeFuncValue   = "[func]"
+)
+
+type store struct {
+	steps      []map[string]interface{}
+	stepMaps   map[string]interface{}
+	vars       map[string]interface{}
+	funcs      map[string]interface{}
+	bindVars   map[string]interface{}
+	parentVars map[string]interface{}
+	useMap     bool // Use map syntax in `steps:`.
+	loopIndex  *int
+}
+
+func (s *store) toNormalizedMap() map[string]interface{} {
+	store := map[string]interface{}{}
+	for k := range s.funcs {
+		store[k] = storeFuncValue
+	}
+	store[storeVarsKey] = s.vars
+	if s.useMap {
+		store[storeStepsKey] = s.stepMaps
+	} else {
+		store[storeStepsKey] = s.steps
+	}
+	for k, v := range s.bindVars {
+		store[k] = v
+	}
+	if s.loopIndex != nil {
+		store[loopCountVarKey] = *s.loopIndex
+	}
+	return store
+}
+
+func (s *store) toMap() map[string]interface{} {
+	store := map[string]interface{}{}
+	for k, v := range s.funcs {
+		store[k] = v
+	}
+	store[storeVarsKey] = s.vars
+	if s.useMap {
+		store[storeStepsKey] = s.stepMaps
+	} else {
+		store[storeStepsKey] = s.steps
+	}
+	if s.parentVars != nil {
+		store[storeParentKey] = s.parentVars
+	}
+	for k, v := range s.bindVars {
+		store[k] = v
+	}
+	if s.loopIndex != nil {
+		store[loopCountVarKey] = *s.loopIndex
+	}
+	return store
+}

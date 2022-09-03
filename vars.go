8d6f6325f0a37e415388db8efe67ab35baadebdd
k2tzumi <katzumi+github@gmail.com>
2022-09-03 13:49:43 +0900

    ✨ Treat json as a template to allow embedding of variables.

--- vars.go
+++ vars.go
@@ -1,23 +1,37 @@
 package runn
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"strings"
+	"text/template"
 
 	"github.com/goccy/go-json"
 )
 
 const varsSupportScheme string = "json://"
 
-func evaluateSchema(value interface{}) (interface{}, error) {
+func evaluateSchema(value interface{}, store map[string]interface{}) (interface{}, error) {
 	switch v := value.(type) {
 	case string:
 		if strings.HasPrefix(v, varsSupportScheme) {
-			byteArray, err := os.ReadFile(v[len(varsSupportScheme):])
+			fn := v[len(varsSupportScheme):]
+			byteArray, err := os.ReadFile(fn)
 			if err != nil {
 				return value, fmt.Errorf("read external files error: %w", err)
-
+			}
+			// If store exists, treat json as a template and replace it with store.
+			if store != nil {
+				tmpl, err := template.New(fn).Parse(string(byteArray))
+				if err != nil {
+					return value, fmt.Errorf("template parse error: %w", err)
+				}
+				buf := new(bytes.Buffer)
+				if err := tmpl.Execute(buf, store); err != nil {
+					return value, fmt.Errorf("template excute error: %w", err)
+				}
+				byteArray = buf.Bytes()
 			}
 			var jsonObj interface{}
 			if err := json.Unmarshal(byteArray, &jsonObj); err != nil {

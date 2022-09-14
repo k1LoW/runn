483860262c636c4e80372f8df2544e8ee15a531a
k1LoW <k1lowxb@gmail.com>
2022-09-14 16:12:23 +0900

    Implement CaptureHTTPRequest

--- capture/runbook.go
+++ capture/runbook.go
@@ -1,13 +1,17 @@
 package capture
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
 	"sync"
 
+	"github.com/goccy/go-json"
 	"github.com/goccy/go-yaml"
 	"github.com/k1LoW/runn"
 )
@@ -22,9 +26,9 @@ type cRunbook struct {
 }
 
 type runbook struct {
-	Desc    string                   `yaml:"desc"`
-	Runners map[string]interface{}   `yaml:"runners,omitempty"`
-	Steps   []map[string]interface{} `yaml:"steps"`
+	Desc    string          `yaml:"desc"`
+	Runners yaml.MapSlice   `yaml:"runners,omitempty"`
+	Steps   []yaml.MapSlice `yaml:"steps"`
 }
 
 func Runbook(dir string) *cRunbook {
@@ -56,11 +60,118 @@ func (c *cRunbook) CaptureEnd(ids []string, bookPath string) {
 	_ = os.WriteFile(p, b, os.ModePerm)
 }
 
-func (c *cRunbook) CaptureHTTPRequest(name string, req *http.Request) {}
+func (c *cRunbook) CaptureHTTPRequest(name string, req *http.Request) {
+	c.setRunner(name, "[THIS IS HTTP RUNNER]")
+	r := c.loadCurrentRunbook()
+	if r == nil {
+		return
+	}
+	endpoint := req.URL.Path
+	if req.URL.RawQuery != "" {
+		endpoint = fmt.Sprintf("%s?%s", endpoint, req.URL.RawQuery)
+	}
+
+	hb := yaml.MapSlice{}
+	// headers
+	contentType := req.Header.Get("Content-Type")
+	if contentType == "" {
+		return
+	}
+	h := yaml.MapSlice{}
+	for k, v := range req.Header {
+		if k == "Content-Type" || k == "Host" {
+			continue
+		}
+		h = append(h, yaml.MapItem{
+			Key:   k,
+			Value: v[0],
+		})
+	}
+	if len(h) > 0 {
+		hb = append(hb, yaml.MapItem{
+			Key:   "headers",
+			Value: h,
+		})
+	}
+
+	// body
+	var bd yaml.MapSlice
+	var (
+		save io.ReadCloser
+		err  error
+	)
+	save, req.Body, err = drainBody(req.Body)
+	if err != nil {
+		return
+	}
+	switch {
+	case save == http.NoBody || save == nil:
+		bd = yaml.MapSlice{
+			{Key: contentType, Value: nil},
+		}
+	case strings.Contains(contentType, "json"):
+		var v interface{}
+		if err := json.NewDecoder(save).Decode(&v); err != nil {
+			return
+		}
+		bd = yaml.MapSlice{
+			{Key: contentType, Value: v},
+		}
+	case contentType == runn.MediaTypeApplicationFormUrlencoded:
+		b, err := io.ReadAll(save)
+		if err != nil {
+			return
+		}
+		vs, err := url.ParseQuery(string(b))
+		if err != nil {
+			return
+		}
+		f := map[string]interface{}{}
+		for k, v := range vs {
+			if len(v) == 1 {
+				f[k] = v[0]
+				return
+			}
+			f[k] = v
+		}
+		bd = yaml.MapSlice{
+			{Key: contentType, Value: f},
+		}
+	default:
+		// case contentType == runn.MediaTypeTextPlain:
+		b, err := io.ReadAll(save)
+		if err != nil {
+			return
+		}
+		bd = yaml.MapSlice{
+			{Key: contentType, Value: string(b)},
+		}
+	}
+	hb = append(hb, yaml.MapItem{
+		Key:   "body",
+		Value: bd,
+	})
+
+	m := yaml.MapItem{Key: strings.ToLower(req.Method), Value: nil}
+	if len(hb) > 0 {
+		m = yaml.MapItem{Key: strings.ToLower(req.Method), Value: hb}
+	}
+
+	step := yaml.MapSlice{
+		{Key: name, Value: yaml.MapSlice{
+			{Key: endpoint, Value: yaml.MapSlice{
+				m,
+			}},
+		}},
+	}
+	r.Steps = append(r.Steps, step)
+}
 
 func (c *cRunbook) CaptureHTTPResponse(name string, res *http.Response) {}
 
-func (c *cRunbook) CaptureGRPCStart(name string, service, method string) {}
+func (c *cRunbook) CaptureGRPCStart(name string, service, method string) {
+	c.setRunner(name, "[THIS IS gRPC RUNNER]")
+}
 
 func (c *cRunbook) CaptureGRPCRequestHeaders(h map[string][]string) {}
 
@@ -76,7 +187,9 @@ func (c *cRunbook) CaptureGRPCResponseTrailers(t map[string][]string) {}
 
 func (c *cRunbook) CaptureGRPCEnd(name string, service, method string) {}
 
-func (c *cRunbook) CaptureDBStatement(name string, stmt string) {}
+func (c *cRunbook) CaptureDBStatement(name string, stmt string) {
+	c.setRunner(name, "[THIS IS DB RUNNER]")
+}
 
 func (c *cRunbook) CaptureDBResponse(name string, res *runn.DBResponse) {}
 
@@ -94,4 +207,48 @@ func (c *cRunbook) SetCurrentIDs(ids []string) {
 
 func (c *cRunbook) Errs() error {
 	return c.errs
+}
+
+func (c *cRunbook) setRunner(name, value string) {
+	r := c.loadCurrentRunbook()
+	if r == nil {
+		return
+	}
+	exist := false
+	for _, rnr := range r.Runners {
+		if rnr.Key.(string) == name {
+			exist = true
+		}
+	}
+	if !exist {
+		r.Runners = append(r.Runners, yaml.MapItem{Key: name, Value: value})
+	}
+}
+
+func (c *cRunbook) loadCurrentRunbook() *runbook {
+	v, ok := c.runbooks.Load(c.currentIDs[0])
+	if !ok {
+		return nil
+	}
+	r, ok := v.(*runbook)
+	if !ok {
+		return nil
+	}
+	return r
+}
+
+// copy from net/http/httputil
+func drainBody(b io.ReadCloser) (r1, r2 io.ReadCloser, err error) {
+	if b == nil || b == http.NoBody {
+		// No copying needed. Preserve the magic sentinel meaning of NoBody.
+		return http.NoBody, http.NoBody, nil
+	}
+	var buf bytes.Buffer
+	if _, err = buf.ReadFrom(b); err != nil {
+		return nil, b, err
+	}
+	if err = b.Close(); err != nil {
+		return nil, b, err
+	}
+	return io.NopCloser(&buf), io.NopCloser(bytes.NewReader(buf.Bytes())), nil
 }

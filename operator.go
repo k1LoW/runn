f96ed8fb97b864cebc84c8ff0479ef0ae8cdab12
k2tzumi <katzumi+github@gmail.com>
2022-09-19 11:20:10 +0900

    ⏪ Revert

--- operator.go
+++ operator.go
@@ -963,7 +963,7 @@ func (o *operator) expand(in interface{}) (interface{}, error) {
 	if reperr != nil {
 		return nil, reperr
 	}
-	var out map[string]interface{}
+	var out interface{}
 	if err := yamlUnmarshal([]byte(e), &out); err != nil {
 		return nil, err
 	}

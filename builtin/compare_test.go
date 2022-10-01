4981e79178f2940ab2b87823edf380dd5270f238
k2tzumi <katzumi+github@gmail.com>
2022-10-01 14:08:25 +0900

    ✅ use ignoreKeys case.

--- builtin/compare_test.go
+++ builtin/compare_test.go
@@ -27,3 +27,24 @@ func TestCompare(t *testing.T) {
 		}
 	}
 }
+
+func TestCompareWithIgnoreKeys(t *testing.T) {
+	tests := []struct {
+		x          interface{}
+		y          interface{}
+		ignorekeys []string
+		want       bool
+	}{
+		{1, 1, []string{"1"}, true},
+		{map[string]interface{}{"foo": "1", "bar": true}, map[string]interface{}{"foo": "1", "bar": true}, []string{}, true},
+		{map[string]interface{}{"foo": "1", "bar": true}, map[string]interface{}{"foo": "1", "bar": false}, []string{"bar"}, true},
+		{map[string]interface{}{"foo": "1", "bar": true}, map[string]interface{}{"foo": "1", "bar": false}, []string{"foo"}, false},
+		{map[string]interface{}{"foo": "1", "bar": true}, map[string]interface{}{}, []string{"foo", "bar"}, true},
+	}
+	for _, tt := range tests {
+		got := Compare(tt.x, tt.y, tt.ignorekeys...)
+		if diff := cmp.Diff(got, tt.want); diff != "" {
+			t.Errorf("%s", diff)
+		}
+	}
+}

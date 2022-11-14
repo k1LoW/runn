27646f349f86e13cce4ab1d3241911f50039208a
k1LoW <k1lowxb@gmail.com>
2022-11-14 12:13:23 +0900

    [skip ci] bonsai

--- errors.go
+++ errors.go
@@ -4,7 +4,9 @@ import "fmt"
 
 type BeforeFuncError struct{ err error }
 
-func (e BeforeFuncError) Error() string { return fmt.Sprintf("before func error: %s", e.err.Error()) }
+func (e BeforeFuncError) Error() string { return fmt.Errorf("before func error: %w", e.err).Error() }
+
+func (e BeforeFuncError) Unwrap() error { return e.err }
 
 func newBeforeFuncError(err error) *BeforeFuncError {
 	return &BeforeFuncError{err: err}
@@ -12,7 +14,9 @@ func newBeforeFuncError(err error) *BeforeFuncError {
 
 type AfterFuncError struct{ err error }
 
-func (e AfterFuncError) Error() string { return fmt.Sprintf("after func error: %s", e.err.Error()) }
+func (e AfterFuncError) Error() string { return fmt.Errorf("after func error: %w", e.err).Error() }
+
+func (e AfterFuncError) Unwrap() error { return e.err }
 
 func newAfterFuncError(err error) *AfterFuncError {
 	return &AfterFuncError{err: err}

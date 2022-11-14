73a53317b7d3d7b01f12f684e7dccd24fd597765
k1LoW <k1lowxb@gmail.com>
2022-11-14 17:08:23 +0900

    Support JSON output of RunN result

--- result.go
+++ result.go
@@ -1,15 +1,27 @@
 package runn
 
+import (
+	"sync"
+	"sync/atomic"
+)
+
 type RunResult struct {
 	Desc string
 	Path string
 	Err  error
 }
 
-func newRunResult(desc, path string, err error) *RunResult {
+type runNResult struct {
+	Total      atomic.Int64
+	Success    atomic.Int64
+	Failed     atomic.Int64
+	Skipped    atomic.Int64
+	RunResults sync.Map
+}
+
+func newRunResult(desc, path string) *RunResult {
 	return &RunResult{
 		Desc: desc,
 		Path: path,
-		Err:  err,
 	}
 }

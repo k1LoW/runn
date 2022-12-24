a4399e534da9a3d7640dc144b62c08d47ed6415c
k1LoW <k1lowxb@gmail.com>
2022-12-24 20:21:15 +0900

    Remove debug code

--- include.go
+++ include.go
@@ -2,7 +2,6 @@ package runn
 
 import (
 	"context"
-	"fmt"
 	"path/filepath"
 )
 
@@ -30,7 +29,6 @@ func (rnr *includeRunner) Run(ctx context.Context, c *includeConfig) error {
 		rnr.operator.thisT.Helper()
 	}
 	ibp := filepath.Join(rnr.operator.root, c.path)
-	fmt.Printf("ibp %s %s\n", rnr.operator.root, c.path)
 	if err := fetchFile(ibp); err != nil {
 		return err
 	}

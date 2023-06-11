814ca6459654b4e8b365da99ef049e4dfd2d6ad1
k1LoW <k1lowxb@gmail.com>
2023-06-11 23:44:00 +0900

    Allow `.yml` in YAML extensions and added extension checking

--- vars_test.go
+++ vars_test.go
@@ -1,33 +1,36 @@
 package runn
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestEvaluateSchema(t *testing.T) {
-	brokenJson, err := os.CreateTemp("", "broken_json")
+	td := t.TempDir()
+	brokenJson, err := os.Create(filepath.Join(td, "broken.json"))
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.Remove(brokenJson.Name())
 	wd, err := os.Getwd()
 	if err != nil {
 		t.Fatal(err)
 	}
 	tests := []struct {
-		value any
-		store map[string]any
-		want  any
-		error bool
+		value   any
+		store   map[string]any
+		want    any
+		wantErr bool
 	}{
 		{1, nil, 1, false},
 		{[]string{"1"}, nil, []string{"1"}, false},
 		{"string", nil, "string", false},
 		{"json://testdata/vars.json", nil, map[string]any{"foo": "test", "bar": float64(1)}, false},
 		{"yaml://testdata/vars.yaml", nil, map[string]any{"foo": "test", "bar": uint64(1), "baz": float64(2.5)}, false},
+		{"yaml://testdata/vars.yml", nil, map[string]any{"foo": "test", "bar": uint64(1), "baz": float64(2.5)}, false},
 		{"json://not_exists.json", nil, "json://not_exists.json", true},
 		{"json://" + brokenJson.Name(), nil, "json://" + brokenJson.Name(), true},
 		{
@@ -48,14 +51,30 @@ func TestEvaluateSchema(t *testing.T) {
 			map[string]any{"foo": "abc\ndef", "bar": "abc\n\ndef"},
 			false,
 		},
+		{
+			"json://testdata/newline.js",
+			map[string]any{},
+			"",
+			true,
+		},
 	}
-	for _, tt := range tests {
-		got, err := evaluateSchema(tt.value, wd, tt.store)
-		if diff := cmp.Diff(got, tt.want, nil); diff != "" {
-			t.Errorf("%s", diff)
-		}
-		if (tt.error && err == nil) || (!tt.error && err != nil) {
-			t.Errorf("no much error")
-		}
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Parallel()
+			got, err := evaluateSchema(tt.value, wd, tt.store)
+			if err != nil {
+				if !tt.wantErr {
+					t.Errorf("unexpected error: %s", err)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Error("want error")
+			}
+			if diff := cmp.Diff(got, tt.want, nil); diff != "" {
+				t.Errorf("%s", diff)
+			}
+		})
 	}
 }

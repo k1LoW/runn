66218266c1d105db4c02fcf848f53164e3fafd2f
k1LoW <k1lowxb@gmail.com>
2023-07-26 16:42:39 +0900

    Generate IDs using file path of runbooks.
    ref: https://github.com/k1LoW/runn/blob/main/docs/designs/id.md

--- id_test.go
+++ id_test.go
@@ -12,6 +12,50 @@ import (
 	"github.com/samber/lo"
 )
 
+func TestGenerateIDsUsingPath(t *testing.T) {
+	tests := []struct {
+		paths            []string
+		seedReversePaths []string
+	}{
+		{
+			[]string{"a.yml", "b.yml", "c.yml"},
+			[]string{"a.yml", "b.yml", "c.yml"},
+		},
+		{
+			[]string{"path/to/a.yml", "path/to/b.yml", "path/to/c.yml"},
+			[]string{"a.yml", "b.yml", "c.yml"},
+		},
+		{
+			[]string{"path/to/bb/a.yml", "path/to/aa/a.yml"},
+			[]string{"a.yml/bb", "a.yml/aa"},
+		},
+		{
+			[]string{"path/to/bb/a.yml", "../../path/to/aa/a.yml"},
+			[]string{"a.yml/bb", "a.yml/aa"},
+		},
+	}
+	for _, tt := range tests {
+		ops := []*operator{}
+		for _, p := range tt.paths {
+			ops = append(ops, &operator{
+				bookPath: p,
+			})
+		}
+		if err := generateIDsUsingPath(ops); err != nil {
+			t.Fatal(err)
+		}
+		for i, o := range ops {
+			want, err := generateID(tt.seedReversePaths[i])
+			if err != nil {
+				t.Fatal(err)
+			}
+			if o.id != want {
+				t.Errorf("want %s, got %s", want, o.id)
+			}
+		}
+	}
+}
+
 func BenchmarkReversePath(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		p := "/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z"

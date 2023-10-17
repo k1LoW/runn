cd155f72b918c03afb781754ce989fb1835c40a8
k1LoW <k1lowxb@gmail.com>
2023-10-18 07:11:17 +0900

    Add test

---
+++ step_test.go
@@ -0,0 +1,42 @@
+package runn
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStepRunbookID(t *testing.T) {
+	s2 := 2
+	s3 := 3
+
+	tests := []struct {
+		s        *step
+		want     string
+		wantMore string
+	}{
+		{
+			&step{idx: s2, key: "s-b", parent: &operator{id: "o-c"}},
+			"o-c",
+			"o-c?step=2",
+		},
+		{
+			&step{idx: s2, key: "s-b", parent: &operator{id: "o-c", parent: &step{idx: s3, key: "s-d", parent: &operator{id: "o-e"}}}},
+			"o-e",
+			"o-e?step=3&step=2",
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			got := tt.s.runbookID()
+			if got != tt.want {
+				t.Errorf("got %v\nwant %v", got, tt.want)
+			}
+			{
+				got := tt.s.runbookIDMore()
+				if got != tt.wantMore {
+					t.Errorf("got %v\nwant %v", got, tt.wantMore)
+				}
+			}
+		})
+	}
+}

32088dcec76491ffec2d5f92dffb66ef33d79740
k1LoW <k1lowxb@gmail.com>
2024-02-22 17:41:19 +0900

    Fix

--- operator_test.go
+++ operator_test.go
@@ -678,7 +678,6 @@ func TestShard(t *testing.T) {
 				cmpopts.IgnoreFields(operator{}, "id"),
 				cmpopts.IgnoreFields(operator{}, "concurrency"),
 				cmpopts.IgnoreFields(operator{}, "mu"),
-				cmpopts.IgnoreFields(operator{}, "kv"),
 				cmpopts.IgnoreFields(cdpRunner{}, "ctx"),
 				cmpopts.IgnoreFields(cdpRunner{}, "cancel"),
 				cmpopts.IgnoreFields(cdpRunner{}, "opts"),

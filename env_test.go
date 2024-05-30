c95d4433a87902b918fc79ea9139e52bda33b5df
k1LoW <k1lowxb@gmail.com>
2024-05-30 23:09:08 +0900

    Add test

---
+++ env_test.go
@@ -0,0 +1,34 @@
+package runn
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestLoadEnvFile(t *testing.T) {
+	t.Setenv("TEST_LOAD_ENV", "")
+	tests := []struct {
+		envs    string
+		wantEnv string
+	}{
+		{"", ""},
+		{"TEST_LOAD_ENV=hoge", "hoge"},
+		{"TEST_LOAD_ENV=hoge\n", "hoge"},
+		{"TEST_LOAD_ENV=hoge\nTEST_LOAD_ENV=fuga", "fuga"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.envs, func(t *testing.T) {
+			path := filepath.Join(t.TempDir(), ".env")
+			if err := os.WriteFile(path, []byte(tt.envs), os.ModePerm); err != nil {
+				t.Fatal(err)
+			}
+			if err := LoadEnvFile(path); err != nil {
+				t.Fatal(err)
+			}
+			if got := os.Getenv("TEST_LOAD_ENV"); got != tt.wantEnv {
+				t.Errorf("got %v\nwant %v", got, tt.wantEnv)
+			}
+		})
+	}
+}

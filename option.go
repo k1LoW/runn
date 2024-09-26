23ebfbca69aacacc14aab9b33cb2d0378aec0b19
k1LoW <k1lowxb@gmail.com>
2024-09-26 23:16:52 +0900

    Use `keys` of expr-lang

--- option.go
+++ option.go
@@ -1248,7 +1248,6 @@ func setupBuiltinFunctions(opts ...Option) []Option {
 		Func("intersect", builtin.Intersect),
 		Func("pick", builtin.Pick),
 		Func("omit", builtin.Omit),
-		Func("keys", builtin.Keys),
 		Func("merge", builtin.Merge),
 		Func("input", func(msg, defaultMsg any) string {
 			return prompter.Prompt(cast.ToString(msg), cast.ToString(defaultMsg))

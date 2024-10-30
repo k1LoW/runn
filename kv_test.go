4c03fdcdee4a15de61a5f7b0d05ff2c9bc65d439
k1LoW <k1lowxb@gmail.com>
2024-10-30 14:02:12 +0900

    Add tests

--- kv_test.go
+++ kv_test.go
@@ -61,10 +61,14 @@ func TestRunNWithKV(t *testing.T) {
 	book := "testdata/book/kv.yml"
 	want := newRunNResult(t, 1, []*RunResult{
 		{
-			ID:          "6fdfa57431f3700a161b5ef02f945a117fd70216",
-			Path:        "testdata/book/kv.yml",
-			Err:         nil,
-			StepResults: []*StepResult{{ID: "6fdfa57431f3700a161b5ef02f945a117fd70216?step=0", Key: "0", Err: nil}},
+			ID:   "6fdfa57431f3700a161b5ef02f945a117fd70216",
+			Path: "testdata/book/kv.yml",
+			Err:  nil,
+			StepResults: []*StepResult{
+				{ID: "6fdfa57431f3700a161b5ef02f945a117fd70216?step=0", Key: "0", Err: nil},
+				{ID: "6fdfa57431f3700a161b5ef02f945a117fd70216?step=1", Key: "1", Err: nil},
+				{ID: "6fdfa57431f3700a161b5ef02f945a117fd70216?step=2", Key: "2", Err: nil},
+			},
 		},
 	})
 	ops, err := Load(book)
@@ -74,6 +78,11 @@ func TestRunNWithKV(t *testing.T) {
 	go func() {
 		time.Sleep(50 * time.Millisecond)
 		ops.SetKV("email", "test@example.com")
+		ops.SetKV("map", map[string]interface{}{
+			"str": "hello",
+			"int": 123,
+		})
+		ops.SetKV("dot.key", "dot.value")
 	}()
 	if err := ops.RunN(ctx); err != nil {
 		t.Error(err)

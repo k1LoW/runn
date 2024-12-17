ae91ea93b433bf40292043722864aa3e4cbadf31
k1LoW <k1lowxb@gmail.com>
2024-12-17 16:46:21 +0900

    Add test for map

--- defer_test.go
+++ defer_test.go
@@ -6,74 +6,90 @@ import (
 )
 
 func TestDeferRun(t *testing.T) {
-	book := "testdata/book/defer.yml"
-	ctx := context.Background()
-	o, err := New(Book(book))
-	if err != nil {
-		t.Fatal(err)
+	tests := []struct {
+		book string
+	}{
+		{"testdata/book/defer.yml"},
+		{"testdata/book/defer_map.yml"},
 	}
-	if err := o.Run(ctx); err == nil {
-		t.Fatal("expected error")
-	}
-	if want := 8; len(o.store.steps) != want {
-		t.Errorf("o.store.steps got %v, want %v", len(o.store.steps), want)
-	}
-	r := o.Result()
-	if want := 8; len(r.StepResults) != want {
-		t.Errorf("r.StepResults got %v, want %v", len(r.StepResults), want)
-	}
+	for _, tt := range tests {
+		t.Run(tt.book, func(t *testing.T) {
+			ctx := context.Background()
+			o, err := New(Book(tt.book))
+			if err != nil {
+				t.Fatal(err)
+			}
+			if err := o.Run(ctx); err == nil {
+				t.Fatal("expected error")
+			}
 
-	t.Run("main steps", func(t *testing.T) {
-		wantResults := []struct {
-			desc    string
-			skipped bool
-			err     bool
-		}{
-			{"step 1", false, false},
-			{"include step", false, false},
-			{"step 2", false, false},
-			{"step 3", false, true},
-			{"step 4", true, false},
-			{"defererd step c", false, false},
-			{"defererd step b", false, true},
-			{"defererd step a", false, false},
-		}
-		for i, want := range wantResults {
-			got := r.StepResults[i]
-			if got.Desc != want.desc {
-				t.Errorf("got %v, want %v", got.Desc, want.desc)
+			if o.useMap {
+				if want := 8; len(o.store.stepMap) != want {
+					t.Errorf("o.store.steps got %v, want %v", len(o.store.steps), want)
+				}
+			} else {
+				if want := 8; len(o.store.steps) != want {
+					t.Errorf("o.store.steps got %v, want %v", len(o.store.steps), want)
+				}
 			}
-			if got.Skipped != want.skipped {
-				t.Errorf("got %v, want %v", got.Skipped, want.skipped)
+			r := o.Result()
+			if want := 8; len(r.StepResults) != want {
+				t.Errorf("r.StepResults got %v, want %v", len(r.StepResults), want)
 			}
-			if (got.Err == nil) == want.err {
-				t.Errorf("got %v, want %v", got.Err, want.err)
-			}
-		}
-	})
 
-	t.Run("include steps", func(t *testing.T) {
-		wantResults := []struct {
-			desc    string
-			skipped bool
-			err     bool
-		}{
-			{"included step 1", false, false},
-			{"included step 2", false, false},
-			{"included defererd step d", false, false},
-		}
+			t.Run("main steps", func(t *testing.T) {
+				wantResults := []struct {
+					desc    string
+					skipped bool
+					err     bool
+				}{
+					{"step 1", false, false},
+					{"include step", false, false},
+					{"step 2", false, false},
+					{"step 3", false, true},
+					{"step 4", true, false},
+					{"defererd step c", false, false},
+					{"defererd step b", false, true},
+					{"defererd step a", false, false},
+				}
+				for i, want := range wantResults {
+					got := r.StepResults[i]
+					if got.Desc != want.desc {
+						t.Errorf("got %v, want %v", got.Desc, want.desc)
+					}
+					if got.Skipped != want.skipped {
+						t.Errorf("got %v, want %v", got.Skipped, want.skipped)
+					}
+					if (got.Err == nil) == want.err {
+						t.Errorf("got %v, want %v", got.Err, want.err)
+					}
+				}
+			})
 
-		for i, want := range wantResults {
-			got := r.StepResults[1].IncludedRunResults[0].StepResults[i]
-			if got.Desc != want.desc {
-				t.Errorf("got %v, want %v", got.Desc, want.desc)
-			}
-			if got.Skipped != want.skipped {
-				t.Errorf("got %v, want %v", got.Skipped, want.skipped)
-			}
-			if (got.Err == nil) == want.err {
-				t.Errorf("got %v, want %v", got.Err, want.err)
-			}
-		}
-	})
+			t.Run("include steps", func(t *testing.T) {
+				wantResults := []struct {
+					desc    string
+					skipped bool
+					err     bool
+				}{
+					{"included step 1", false, false},
+					{"included step 2", false, false},
+					{"included defererd step d", false, false},
+				}
+
+				for i, want := range wantResults {
+					got := r.StepResults[1].IncludedRunResults[0].StepResults[i]
+					if got.Desc != want.desc {
+						t.Errorf("got %v, want %v", got.Desc, want.desc)
+					}
+					if got.Skipped != want.skipped {
+						t.Errorf("got %v, want %v", got.Skipped, want.skipped)
+					}
+					if (got.Err == nil) == want.err {
+						t.Errorf("got %v, want %v", got.Err, want.err)
+					}
+				}
+			})
+		})
+	}
 }
